0db4661afbd5c785aa7ff0730ed1956f7ff00f19
elias.alexandre <elliasallex@gmail.com>
2023-11-05 18:45:37 -0300

    Loading effect in search page

--- src/app/(store)/search/loading.tsx
+++ src/app/(store)/search/loading.tsx
@@ -1,3 +1,25 @@
+"use client";
+
+import { useSearchParams } from "next/navigation";
+
+import { Skeleton } from "@/components/skeleton";
+
 export default function HomeLoading() {
-  return <h1>search loading...</h1>;
+  const searchParams = useSearchParams();
+
+  const query = searchParams.get("q");
+
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-sm">
+        Resultados para: <span className="font-semibold">{query ?? ""}</span>
+      </p>
+
+      <div className="grid grid-cols-3 gap-4">
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+        <Skeleton className="h-[380px]" />
+      </div>
+    </div>
+  );
 }
